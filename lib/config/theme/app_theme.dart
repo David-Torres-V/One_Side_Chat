d2dea841997dd69719b2124bb4ffee93938ff9c9
David Antonio Torres Vilchez <david6t.v2003@gmail.com>
2024-03-19 01:01:32 -0500

    add config theme

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const Color _customColor = Color(0xFFFFFFFF);
+
+const List<Color> _colorScheme=[
+  _customColor,
+  Colors.black,
+  Colors.amber,
+  Colors.cyan,
+];
+
+class AppTheme{
+
+  int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor >= 0 && selectedColor <= _colorScheme.length,
+            'El número debe ser menor a : ${_colorScheme.length}');
+
+  ThemeData theme (){
+    return ThemeData(useMaterial3: true, colorSchemeSeed: _colorScheme[selectedColor],brightness: Brightness.dark);
+  }
+}
